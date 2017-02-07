b528cbc215f02a64a70056ee08221b23dea9ef2f
leejaelin <leejaelin@naver.com>
2017-02-07 22:28:53 +0900

    [MyStl][추가] 폴더 구성 변경

---
+++ src/MyStl/MyStl/Src/MyStl/Iterator/Stock.h
@@ -0,0 +1,26 @@
+#pragma once
+
+template<typename T>
+struct Stock
+{
+public:
+	Stock()
+	{
+		init();
+	}
+	Stock(T &t)
+	{
+		init();
+		value = t;
+	}
+	Stock * prev;
+	Stock * next;
+	T value;
+
+private:
+	void init()
+	{
+		prev = nullptr;
+		next = nullptr;
+	}
+};
